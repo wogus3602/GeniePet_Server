89f083df5881fc90e65fb44cf3168123fcfc1a83
solone313@naver.com <solone313@naver.com>
2019-08-29 17:56:31 +0900

    ordering rating

--- Django/imageproject/reco/models.py
+++ Django/imageproject/reco/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class feed(models.Model):
     name = models.CharField(max_length=30)
     price = models.CharField(max_length=15)
+    rating = models.IntegerField(default=0)
     create_date = models.DateTimeField(auto_created=True, auto_now_add=True)
+
+    class Meta:
+        ordering = ['-rating', ]
--- Django/imageproject/reco/serializers.py
+++ Django/imageproject/reco/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class FeedSerializer(serializers.ModelSerializer):
     class Meta:
         model = feed
-        fields = ['id','name', 'price']
+        fields = ['id','name', 'price','rating']
