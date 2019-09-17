4590a78ecd771117226059e536791ff021011b55
solone313@naver.com <solone313@naver.com>
2019-09-17 16:38:34 +0900

    change url

--- Django/imageproject/reco/urls.py
+++ Django/imageproject/reco/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
 from django.conf.urls import include, url
-
+from keras.models import load_model
 router = DefaultRouter()
 router.register('feed', views.FeedViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('pot/upload/',views.post),
 	]
