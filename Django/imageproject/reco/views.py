af2bf12c11f4917300f4ef4655fa57714cd30e1d
solone313@naver.com <solone313@naver.com>
2019-09-17 16:29:05 +0900

    deep learning

--- Django/imageproject/reco/views.py
+++ Django/imageproject/reco/views.py
@@ -2,8 +2,39 @@ from django.shortcuts import render
 from .models import feed
 from .serializers import FeedSerializer
 from rest_framework import viewsets
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+from django.http import HttpResponse, JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 class FeedViewSet(viewsets.ModelViewSet):
     queryset = feed.objects.all()
     serializer_class = FeedSerializer
+
+
+model = load_model('model.h5')
+graph = tf.get_default_graph()
+
+@csrf_exempt
+def post(request):
+    global graph
+    with graph.as_default():
+        test_image = request.FILES['model_pic']
+        img = Image.open(test_image)
+        img = img.convert("RGB")
+        img = img.resize((224,224))
+        data = np.asarray(img)
+        X = np.array(data)
+        X = X.astype("float") / 256
+        X = X.reshape(-1, 224, 224,3)
+        
+        categories = ['GermanShepherd', 'GoldenRetriever', 'SiberianHusky']
+        pred = model.predict(X)
+        print(pred)
+        result = [np.argmax(value) for value in pred]  
+        print('New data category : ',categories[result[0]])
+        return HttpResponse(categories[result[0]])
