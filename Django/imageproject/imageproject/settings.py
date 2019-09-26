5060bff43dae3a0d7c99f859b3fc55fa967be945
solone313@naver.com <solone313@naver.com>
2019-09-26 11:20:46 +0900

    connect postgresql

--- Django/imageproject/imageproject/settings.py
+++ Django/imageproject/imageproject/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'imageproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': '000000',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
